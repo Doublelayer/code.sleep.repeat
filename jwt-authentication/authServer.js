f5455ca52a89b9fe8fab1400b1e946bb6f0f28ea
Doublelayer <florin.hamann@web.de>
2019-10-03 19:51:45 +0200

    add .env.sample to jwt auth repo

--- jwt-authentication/authServer.js
+++ jwt-authentication/authServer.js
@@ -14,8 +14,10 @@ let refreshTokens = [];
 
 app.post("/token", (req, res) => {
   const refreshToken = req.body.token;
+
   if (refreshToken == null) return res.sendStatus(401);
   if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     const accsessToken = generateAccessToken({ name: user.name });
