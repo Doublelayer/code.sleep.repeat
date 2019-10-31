8604b6f656ad08ed04222c05a82aa63f622f6aef
Doublelayer <florin.hamann@web.de>
2019-10-31 16:16:02 +0100

    add web dev stater

---
+++ web-dev-starter/src/assets/js/java.js
@@ -0,0 +1,2 @@
+console.log("Florin");
+const a = "Hi";
---
+++ web-dev-starter/template/header.php
@@ -0,0 +1,33 @@
+<head>
+        <meta charset="utf-8">
+        <title>S+F GOES Hamburg</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
+
+        <!-- Favicons -->
+        <link href="img/Real_Estate_Outline_red-17-32.png" rel="icon">
+        <link href="img/Real_Estate_Outline_red-17-32.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+        <!-- Bootstrap CSS File -->
+        <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Libraries CSS Files -->
+        <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+        <!-- Main Stylesheet File -->
+        <link href="css/style.css" rel="stylesheet">
+
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+
+        <script src="js/item.js"></script>
+
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" >
+
+    </head>
