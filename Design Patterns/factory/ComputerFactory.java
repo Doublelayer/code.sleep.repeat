8cc03b6d59aab9127aa5a8615961f1ef24dec1d5
Doublelayer <florin.hamann@web.de>
2020-08-23 18:15:23 +0200

    rmv cicd

--- Design Patterns/factory/ComputerFactory.java
+++ Design Patterns/factory/ComputerFactory.java
@@ -1,10 +1,13 @@
 package factory;
 
 public class ComputerFactory {
-    public static Computer getComputer(String type, String ram, String hdd, String cpu){
-        if ("Laptop".equalsIgnoreCase(type)) return new Laptop(ram, hdd, cpu);
-        else if ("Server".equalsIgnoreCase(type)) return new Server(ram, hdd, cpu);
-
-        return null;
+    public static Computer getComputer(String type, String ram, String hdd, String cpu) {
+        if ("Laptop".equalsIgnoreCase(type)) {
+            return new Laptop(ram, hdd, cpu);
+        } else if (("Server".equalsIgnoreCase(type))) {
+            return new Server(ram, hdd, cpu);
+        } else {
+            return null;
+        }
     }
 }
